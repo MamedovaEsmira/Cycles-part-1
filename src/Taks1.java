5b1d7582677aa1c331154224e520970850668c14
mamedovaesmira <mamedovaesmira88@gmail.com>
2022-10-05 01:14:07 +0300

    ДЗ Сделано

--- src/Taks1.java
+++ src/Taks1.java
@@ -1,2 +1,21 @@
 public class Taks1 {
+    public static void main(String[] args) {
+        System.out.println("Задача1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Задача2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Задача3");
+        for (int i = 0; i <= 17; i=i+2) {
+            System.out.println(i);
+        }
+        System.out.println("Задача4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+
+        }
+    }
 }
